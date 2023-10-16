e6319ccfcbb7e9ec90f24e9324d3cf79113512d9
Jared Israel Tovar Covarrubias <71293482+I20050041@users.noreply.github.com>
2023-10-16 12:36:02 -0600

    Add files via upload

---
+++ Cliente/Cliente/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Cliente
+{
+    static void Main()
+    {
+        string serverIp = "127.0.0.1";
+        int serverPort = 12345; 
+
+
+        TcpClient client = new TcpClient(serverIp, serverPort);
+
+        NetworkStream stream = client.GetStream();
+
+        string message = "Hola";
+        byte[] data = Encoding.ASCII.GetBytes(message);
+
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Mensaje enviado al servidor.");
+
+        data = new byte[1024];
+        int bytesRead = stream.Read(data, 0, data.Length);
+        string response = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+        Console.WriteLine("Respuesta del servidor: " + response);
+
+        client.Close();
+    }
+}
---
+++ Servidor/Servidor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Servidor
+{
+    static void Main()
+    {
+        
+        string ipAddress = "127.0.0.1"; 
+        int port = 12345; 
+
+        TcpListener listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+
+        listener.Start();
+        Console.WriteLine($"Servidor conectado a {ipAddress}:{port}");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Conexión entrante...");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = new byte[1024];
+            int bytesRead = stream.Read(data, 0, data.Length);
+            string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+            Console.WriteLine($"Mensaje recibido: {message}");
+
+            string response = "Mensaje recibido: " + message;
+            byte[] responseData = Encoding.ASCII.GetBytes(response);
+
+            stream.Write(responseData, 0, responseData.Length);
+            Console.WriteLine("Respuesta enviada.");
+
+            client.Close();
+        }
+    }
+}
